a29cb290515998516b5b0a4f7be552e4a95e7fd9
alainabuzas <alaina.buzas@gmail.com>
2017-06-02 11:53:48 -0700

    smushed home and login

--- public/app/app.js
+++ public/app/app.js
@@ -8,29 +8,25 @@ app.config([
             $urlRouterProvider.otherwise('/404');
             // $httpProvider.interceptors.push('AuthInterceptor');
 
-        $stateProvider
-            .state('home', {
-                url: '/',
-                templateUrl: 'views/home.html'
-            })
-            .state('login', {
-                url: '/login',
-                templateUrl: 'views/userLogin.html',
-                controller: 'LoginCtrl'
-            })
-            .state('signup', {
-                url: '/signup',
-                templateUrl: 'views/userSignup.html',
-                controller: 'SignupCtrl'
-            })
-            .state('profile', {
-                url: '/profile/:id',
-                templateUrl: 'views/profile.html',
-                controller: 'ProfileCtrl'
-            })
-            .state('404', {
-                url: '/404',
-                templateUrl: 'views/404.html'
+            $stateProvider
+                .state('home', {
+                    url: '/',
+                    templateUrl: 'views/userLogin.html',
+                    controller: 'LoginCtrl'
+                })
+                .state('signup', {
+                    url: '/signup',
+                    templateUrl: 'views/userSignup.html',
+                    controller: 'SignupCtrl'
+                })
+                .state('profile', {
+                    url: '/profile/:id',
+                    templateUrl: 'views/profile.html',
+                    controller: 'ProfileCtrl'
+                })
+                .state('404', {
+                    url: '/404',
+                    templateUrl: 'views/404.html'
 
                 });
 
