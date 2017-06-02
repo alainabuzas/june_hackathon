0d51f0d21c5e229d7525ceb4417a297f862ed787
alainabuzas <alaina.buzas@gmail.com>
2017-06-02 10:41:36 -0700

    stuff

--- public/app/app.js
+++ public/app/app.js
@@ -6,32 +6,37 @@ app.config([
     '$locationProvider',
     function($stateProvider, $urlRouterProvider, $locationProvider) {
         $urlRouterProvider.otherwise('/404');
+        // $httpProvider.interceptors.push('AuthInterceptor');
 
         $stateProvider
             .state('home', {
                 url: '/',
                 templateUrl: 'views/home.html'
             })
-            // .state('login', {
-            //     url: '/login',
-            //     templateUrl: 'views/userLogin.html'/*,
-            //     controller: 'LoginCtrl'*/
-            // })
-            // .state('signup', {
-            //     url: '/signup',
-            //     templateUrl: 'views/userSignup.html'/*,
-            //     controller: 'SignupCtrl'*/
-            // })
-            // .state('profile', {
-            //     url: 'profile',
-            //     templateUrl: 'views/profile.html'/*,
-            //     controller: 'ProfileCtrl'*/
-            // });
+            .state('login', {
+                url: '/login',
+                templateUrl: 'views/userLogin.html',
+                controller: 'LoginCtrl'
+            })
+            .state('signup', {
+                url: '/signup',
+                templateUrl: 'views/userSignup.html',
+                controller: 'SignupCtrl'
+            })
+            .state('profile', {
+                url: 'profile',
+                templateUrl: 'views/profile.html',
+                controller: 'ProfileCtrl'
+            })
+            .state('404', {
+                url: '/404',
+                templateUrl: 'views/404.html'
+            });
 
         $locationProvider.html5Mode(true);
     }
 ])
 
 .config(['$httpProvider', function($httpProvider) {
-    //$httpProvider.interceptors.push('AuthInterceptor');
+    // $httpProvider.interceptors.push('AuthInterceptor');
 }]);
--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,5 +1,28 @@
 angular.module('MainCtrls', ['MainServices'])
-	.controller('SignupCtrl', ['$scope', '$http', '$location', 'Auth', 'Alerts', function($scope, $http, $location, Auth, Alerts) {
+    .controller('NavCtrl', ['$scope', '$http', 'Auth', 'Alerts', '$state', function($scope, $http, Auth, Alerts, $state) {
+        $scope.Auth = Auth;
+        $scope.status = {
+            isopen: false
+        };
+
+        $scope.logout = function() {
+            Alerts.add('danger', 'You logged out');
+            Auth.removeToken();
+            $state.go('home');
+        };
+
+        $scope.toggled = function(open) {};
+
+        $scope.toggleDropdown = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.status.isopen = !$scope.status.isopen;
+        };
+    }])
+    .controller('AlertsCtrl', ['$scope', '$http', 'Alerts', function($scope, $http, Alerts) {
+        $scope.Alerts = Alerts;
+    }])
+    .controller('SignupCtrl', ['$scope', '$http', '$location', 'Auth', 'Alerts', function($scope, $http, $location, Auth, Alerts) {
         $scope.user = {
             name: '',
             email: '',
@@ -25,20 +48,20 @@ angular.module('MainCtrls', ['MainServices'])
             });
         }
     }])
-  .controller('LoginCtrl', ['$scope', '$http', '$location', 'Auth', 'Alerts', function($scope, $http, $location, Auth, Alerts) {
-      $scope.user = {
-          email: '',
-          password: ''
-      };
-      $scope.userLogin = function() {
-          $http.post('/api/auth', $scope.user).then(function success(res) {
-              Auth.saveToken(res.data);
-              Alerts.add('success', 'Logged in!');
-              console.log('Token:', res.data);
-              $location.path('/');
-          }, function error(res) {
-              Alerts.add('danger', 'Incorrect email/password');
-              console.log(res);
-          });
-      }
-  }])
+    .controller('LoginCtrl', ['$scope', '$http', '$location', 'Auth', 'Alerts', function($scope, $http, $location, Auth, Alerts) {
+        $scope.user = {
+            email: '',
+            password: ''
+        };
+        $scope.userLogin = function() {
+            $http.post('/api/auth', $scope.user).then(function success(res) {
+                Auth.saveToken(res.data);
+                Alerts.add('success', 'Logged in!');
+                console.log('Token:', res.data);
+                $location.path('/');
+            }, function error(res) {
+                Alerts.add('danger', 'Incorrect email/password');
+                console.log(res);
+            });
+        }
+    }])
