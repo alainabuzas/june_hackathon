a5a4468ea734fe972204e70f033f3db5be6906fd
markdgold <mgoldstein@utexas.edu>
2017-06-02 10:14:31 -0700

    angular working again

--- public/app/app.js
+++ public/app/app.js
@@ -1,37 +1,36 @@
-var app = angular.module('BBQApp', ['ui.router', 'ui.bootstrap', 'MainCtrls']);
+var app = angular.module('BBQApp', ['ui.router', 'ui.bootstrap']);
 
 app.config([
     '$stateProvider',
     '$urlRouterProvider',
     '$locationProvider',
     function($stateProvider, $urlRouterProvider, $locationProvider) {
-
         $urlRouterProvider.otherwise('/404');
 
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: 'app/views/home.html'
+                templateUrl: 'views/home.html'
             })
-            .state('login', {
-                url: '/login',
-                templateUrl: 'app/views/userLogin.html',
-                controller: 'LoginCtrl'
-            })
-            .state('signup', {
-                url: '/signup',
-                templateUrl: 'app/views/userSignup.html',
-                controller: 'SignupCtrl'
-            })
-            .state('profile', {
-                url: 'profile',
-                templateUrl: 'app/views/profile.html',
-                controller: 'ProfileCtrl'
-            });
+            // .state('login', {
+            //     url: '/login',
+            //     templateUrl: 'views/userLogin.html'/*,
+            //     controller: 'LoginCtrl'*/
+            // })
+            // .state('signup', {
+            //     url: '/signup',
+            //     templateUrl: 'views/userSignup.html'/*,
+            //     controller: 'SignupCtrl'*/
+            // })
+            // .state('profile', {
+            //     url: 'profile',
+            //     templateUrl: 'views/profile.html'/*,
+            //     controller: 'ProfileCtrl'*/
+            // });
         $locationProvider.html5Mode(true);
     }
 ])
 
 .config(['$httpProvider', function($httpProvider) {
-    $httpProvider.interceptors.push('AuthInterceptor');
+    //$httpProvider.interceptors.push('AuthInterceptor');
 }]);
