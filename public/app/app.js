699477bf7c45b71079bc7a0988dfaf53192e896b
alainabuzas <alaina.buzas@gmail.com>
2017-06-02 10:13:36 -0700

    update

--- public/app/app.js
+++ public/app/app.js
@@ -11,21 +11,21 @@ app.config([
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: 'app/views/home.html'
+                templateUrl: 'views/home.html'
             })
             .state('login', {
                 url: '/login',
-                templateUrl: 'app/views/userLogin.html',
+                templateUrl: 'views/userLogin.html',
                 controller: 'LoginCtrl'
             })
             .state('signup', {
                 url: '/signup',
-                templateUrl: 'app/views/userSignup.html',
+                templateUrl: 'views/userSignup.html',
                 controller: 'SignupCtrl'
             })
             .state('profile', {
                 url: 'profile',
-                templateUrl: 'app/views/profile.html',
+                templateUrl: 'views/profile.html',
                 controller: 'ProfileCtrl'
             });
         $locationProvider.html5Mode(true);
